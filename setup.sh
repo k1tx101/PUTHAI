912f59896a0a22f9ad3712c9fc811111df08aa6a
k1tx <72155243+k1tx101@users.noreply.github.com>
2026-02-09 05:49:33 +0700

    Refactor nvm loading in setup.sh
    Updated setup script to use 'source' instead of '.' for loading nvm.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
 apt update -y
 apt upgrade -y
 apt install systemd-standalone* -y
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash 
-\. "$HOME/.nvm/nvm.sh"
-nvm install 24 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+source "$HOME/.nvm/nvm.sh"
+nvm install 24
 npm i -g openclaw
+sleep infinity
