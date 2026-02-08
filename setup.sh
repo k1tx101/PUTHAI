34be35202184016e00d522d3c2fa617d7f4b027f
k1tx <72155243+k1tx101@users.noreply.github.com>
2026-02-09 05:28:15 +0700

    Refactor setup.sh for better readability

---
+++ setup.sh
@@ -0,0 +1,7 @@
+apt update
+apt upgrade
+apt install systemd-standalone*
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash 
+\. "$HOME/.nvm/nvm.sh"
+nvm install 24 
+npm i -g openclaw
